f6d004dfa8edf1811b6784d47a6276297d94a526
2863862963872963 <119198880+2863862963872963@users.noreply.github.com>
2024-11-02 18:42:50 +0700

    Update 777.lua

--- Cc/777.lua
+++ Cc/777.lua
@@ -1,6 +1,30 @@
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer 
+local Character = Player.Character or Player.CharacterAdded:Wait()  -- Waits if the character hasn’t spawned yet
 
-game:GetService("Players").LocalPlayer.Character:FindFirstChild("Old Fishing Rod").Replicator:FireServer({["Action"] = "Attack", ["Data"] = { ["Alpha"] = 1, ["ResponseTime"] = 0 }})
-game:GetService("ReplicatedStorage").Events.FishPull:FireServer("Right")
+function AutoFish()
+    while getgenv().AutoFish do
+        task.wait()
+        local IsCatching = Character:GetAttribute("Catching")
+
+        if not IsCatching then
+            repeat
+                task.wait()
+                game:GetService("Players").LocalPlayer.Character:FindFirstChild("Old Fishing Rod").Replicator:FireServer({
+                    ["Action"] = "Attack",
+                    ["Data"] = { ["Alpha"] = 1, ["ResponseTime"] = 0 }
+                })
+            until Character:GetAttribute("Catching")
+        end
+
+        if Character:GetAttribute("Catching") then
+            repeat
+                task.wait()
+                game:GetService("ReplicatedStorage").Events.FishPull:FireServer("Right")
+            until not Character:GetAttribute("Catching")
+        end
+    end
+end
 
 local AuraIS = loadstring(game:HttpGet("https://raw.githubusercontent.com/2863862963872963/2862763762763819374946311/refs/heads/main/Agoodo82726277.lua"))()
 
@@ -25,5 +49,9 @@ local FishSeclection = MainTab:CreateSection("Fishing" , "Normal")
 local AutoFishTog = FishSeclection:CreateToggle("Radio", {
     Name = "Auto Fish 🐟",
     Callback = function(Value)
+            getgenv().AutoFish = Value
+            if Value == true then
+                AutoFish()
+            end
     end,
 })
